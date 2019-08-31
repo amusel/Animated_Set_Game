52333782e29ac8349c8ca1322efde0f3bed2e289
amusel <a.musel98@gmail.com>
2019-08-31 14:37:56 +0300

    initial commit

---
+++ Animated_Set_Game/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Animated_Set_Game
+//
+//  Created by Artem Musel on 8/30/19.
+//  Copyright © 2019 Artem Musel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
